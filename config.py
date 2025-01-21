e01123fba0f5c47a8119738fc0b0834d57eb5545
ɪɴᴠᴀʟɪᴅ <116873243+ifeelscam@users.noreply.github.com>
2025-01-21 18:50:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,20 +9,20 @@ from logging.handlers import RotatingFileHandler
 
 
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7075725841:AAEM_SSLJ4Inoe9161iylHbiVgHvXwaK98Q")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7714512079:AAEbpWPhkm7HFqArHFNrXQ9pokXeRTgjZjU")
 APP_ID = int(os.environ.get("APP_ID", "24371796"))
 API_HASH = os.environ.get("API_HASH", "8121c78f4b8b31e88cc2623d1277338d")
 
 
-OWNER = os.environ.get("OWNER", "God_Of_Genjutsue") #Owner username
-OWNER_ID = int(os.environ.get("OWNER_ID", "5405110137")) #Owner user id
+OWNER = os.environ.get("OWNER", "Tharun_stryker") #Owner username
+OWNER_ID = int(os.environ.get("OWNER_ID", "6586630448")) #Owner user id
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://hamzann:hamza00@cluster0.id2lo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "TA_Links_Bot")
+DB_NAME = os.environ.get("DB_NAME", "Aira_Anime_Bot")
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002149354979"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002146121321"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002146121321"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002189440226"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002189440226"))
 
 
 SECONDS = int(os.getenv("SECONDS", "1200")) # auto delete in seconds
@@ -33,9 +33,11 @@ PORT = os.environ.get("PORT", "8080")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 
+START_PIC = os.environ.get("START_PIC", "https://envs.sh/gqj.jpg")
 
 
-START_MSG = os.environ.get("START_MESSAGE", "<b>👋🏻 Hey {mention}\n\nI Am Tamil Anime File Store Bot 🍀\nHere You Get All Tamil Animes @TA_Links🍂.</b>")
+
+START_MSG = os.environ.get("START_MESSAGE", "<b>👋🏻 Hey {mention}\n\nI Am Tamil Anime File Store Bot 🍀\nHere You Get All Tamil Animes @HunterXHunter_Tamil🍂.</b>")
 
 try:
     ADMINS=[7085541484]
@@ -54,7 +56,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot ! \n 👨‍💻 Contact Here : @TA_Links_Owner_Bot"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot ! \n 👨‍💻 Join : @OutlawBots"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(7085541484)
