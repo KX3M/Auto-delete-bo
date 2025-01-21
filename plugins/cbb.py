6a60aee98a66ac83f89294ee18ef4b0f0df89a70
KX3M <163962831+KX3M@users.noreply.github.com>
2025-01-21 22:31:03 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,11 +11,11 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>👨‍💻 𝘋𝘦𝘷𝘭𝘰𝘱𝘦𝘳 :</b> <a href='https://t.me/ifeelscam'>sʜᴀɪᴋʜ ᴀʟɪ</a> \n<b> 🤖 𝘊𝘳𝘦𝘢𝘵𝘰𝘳 :</b> <a href='t.me/InkaLinks'> ᴄʜɪᴘs</a>",
+            text = f"<b>👨‍💻 𝘋𝘦𝘷𝘭𝘰𝘱𝘦𝘳 :</b> <a href='https://t.me/outlawbots'>sʜᴀɪᴋʜ ᴀʟɪ</a> \n<b> 🤖 𝘊𝘳𝘦𝘢𝘵𝘰𝘳 :</b> <a href='t.me/faony'> ᴄʜɪᴘs</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [ [ InlineKeyboardButton(" Source code ", url="https://t.me/+NeqCUg-QDxo2Nzll"),
-                  InlineKeyboardButton("Bot Channel" , url= "https://t.me/Publicfille")],
+                  InlineKeyboardButton("Bot Channel" , url= "https://t.me/outlawbots")],
                     [
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
